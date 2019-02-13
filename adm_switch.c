831f9f1f7fdacb777692425f16fa52643d00c795
yamori813 <yamori813@yahoo.co.jp>
2019-02-13 11:02:03 +0900

    add getphyst

--- adm_switch.c
+++ adm_switch.c
@@ -3,34 +3,49 @@
  *
  */
 
-#define	PHY_cntl1	0xb2000068
-#define	PHY_cntl2	0xb200006c
+#define	PHY_St		0xb2000014
+#define	PHY_cntl0	0xb2000068
+#define	PHY_cntl1	0xb200006c
 
 #define	RC		(1 << 14)
 #define	WC		(1 << 13)
 #define	ROR		2
 #define WOD		1
 
+unsigned long physt()
+{
+unsigned long *lptr;
+
+	lptr = (unsigned long *)PHY_St;
+
+	return *lptr;
+}
+
+/* This is only valid mdio for GMII. Not work for Switch PHY. */
+
 int readmdio(unsigned int addr, unsigned int reg, unsigned int *dat)
 {
 unsigned long *lptr;
+unsigned long cntl1;
 int count;
 
-	lptr = (unsigned long *)PHY_cntl1;
+	lptr = (unsigned long *)PHY_cntl0;
 
 	*lptr = RC | (reg << 8) | addr;
 
-	lptr = (unsigned long *)PHY_cntl2;
+	lptr = (unsigned long *)PHY_cntl1;
 
 	count = 0;
-	while (*lptr & ROR == 0) {
-		udelay(1);
+	cntl1 = *lptr;
+	while ((cntl1 & ROR) != ROR) {
+		delay_ms(1);
 		++count;
 		if (count == 100)
 			return 0;
+		cntl1 = *lptr;
 	}
 
-	*dat = *lptr >> 16;
+	*dat = cntl1 >> 16;
 
 	return 1;
 }
