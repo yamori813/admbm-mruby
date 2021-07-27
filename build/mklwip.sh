4f7c9e71040778c368ac34c76e534c58a803145a
Hiroki Mori <yamori813@yahoo.co.jp>
2021-07-28 08:31:08 +0900

    add build library script

---
+++ build/mklwip.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+LWIP=lwip-2.1.2
+
+cd work
+
+rm -rf ${LWIP}
+
+unzip ${LWIP}.zip
+
+START=`date '+%s'`
+
+cp -r ../lwip ${LWIP}/mips4kel
+
+cd ${LWIP}/mips4kel;make
+
+END=`date '+%s'`
+
+TIME=`expr ${END} - ${START}`
+
+echo "${TIME} sec"
